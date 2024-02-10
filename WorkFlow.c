aaf73cc8c8a9c67873aae969e69f4b6902fbafad
wallaclement <rudraksh.sks007@gmail.com>
2024-02-10 19:57:18 +0530

    Create a C file

---
+++ WorkFlow.c
@@ -0,0 +1,16 @@
+/*  Work Flow
+local git
+
+          GitHub repo
+              |
+            clone
+              |
+            changes
+              |
+             add
+              |
+            commit
+              |
+            push
+
+*/ 
